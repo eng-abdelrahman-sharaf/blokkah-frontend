4e7d434f17df4820d8d5647b1d435123be421951
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-11 14:32:33 +0300

    add error colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -34,12 +34,31 @@ const config: Config = {
         '800': '#00192C',
         '900': '#00101D'
       },
+      'Error': {
+        '25': 'FFFBFA',
+        '50': '#FEF3F2',
+        '100': '#FEE4E2',
+        '200': '#FECDCA',
+        '300': '#FDA29B',
+        '400': '#F97066',
+        '500': '#F04438',
+        '600': '#D92D20',
+        '700': '#B42318',
+        '800': '#912018',
+        '900': '#7A271A'
+      }
+    },
+    backgroundColor: {
+      'bg-white': '#FFFFFF'
     },
     boxShadow: {
       'inputBoxShadow': '0px 1px 2px 0px rgba(16, 24, 40, 0.05)',
       'inputFocusedBoxShadow': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px #E3F6FF'
     },
     extend: {
+      fontSize: {
+        sm: ['0.875rem', '1.25rem'],
+      },
     },
   },
   plugins: [],
