4a4eff7743857b85d4d0eada0632f423837549a6
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-15 01:48:28 +0300

    Fix: logo width and height

--- src/components/CopyRightBadge.tsx
+++ src/components/CopyRightBadge.tsx
@@ -5,9 +5,7 @@ const CopyRightBadge = () => {
   return (
     <div className='flex items-center justify-center gap-2 px-6 py-3 mx-auto w-fit rounded-full bg-Gray-50 border border-Gray-300'>
       <p className='text-sm font-semibold text-Gray-700 select-none'>Created by</p>
-      <div className='max-w-[57px] max-h-[28px] w-full h-full overflow-hidden'>
-        <Logo />
-      </div>
+      <Logo width={57} height={28} />
     </div>
   )
 }
