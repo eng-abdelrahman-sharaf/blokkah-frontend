2ad19b651556834beaba80017da6074cc417cfb9
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-14 04:20:52 +0300

    Login Form UI and Logic

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -10,10 +10,13 @@ import { Eye, EyeOff, Lock, Mail } from '@/components/icons';
 import { useAuthContext } from '@/context/AuthContext';
 
 import { adminDashboardLoginHandler } from '@/actions';
+import { Button } from '@/components/UI/Button';
+import Link from 'next/link';
 
 const LoginForm = () => {
     const { email, setEmail } = useAuthContext();
     const [showPassword, setShowPassword] = useState(false);
+
     return (
         <div className='main-form-box-wrapper'>
             <AuthFormsHeader formType='login' />
@@ -26,8 +29,8 @@ const LoginForm = () => {
                         id='login_email_input'
                         label='Email'
                         placeholder='admin@blokka.io'
-                        type={InputType.email}
-                        startIcon={<Mail />}
+                        inputType={InputType.email}
+                        startComponent={<Mail />}
                         name='email-input'
                         value={email}
                         onChange={(event) => setEmail(event.target.value)}
@@ -36,17 +39,34 @@ const LoginForm = () => {
                         id='login_password_input'
                         label='Password'
                         placeholder='Enter Your Password'
-                        type={showPassword ? InputType.text : InputType.password}
-                        startIcon={<Lock />}
-                        endIcon={showPassword ? <EyeOff /> : <Eye />}
+                        inputType={showPassword ? InputType.text : InputType.password}
+                        startComponent={<Lock />}
+                        endComponent={
+                            <div onClick={() => setShowPassword(prev => !prev)} className='cursor-pointer'>
+                                {showPassword ? <EyeOff /> : <Eye />}
+                            </div>
+                        }
                         name='password-input'
-                        onEndIconClick={() => setShowPassword(!showPassword)}
                     />
+                    <div className='flex items-center justify-between w-full mt-1'>
+                        <label htmlFor="remember-me-input" className='flex items-center gap-2 text-sm font-medium text-Gray-700 select-none cursor-pointer'>
+                            <input type="checkbox" name="remember-me" id="remember-me-input" className='border border-Gray-300 bg-white cursor-pointer' />
+                            Remember me
+                        </label>
+                        <Link href='/forget-password' className='text-sm font-semibold text-Brand-700'>Forget password?</Link>
+                    </div>
                 </div>
-                <button type='submit' className='w-full h-[48px] rounded-lg bg-Gray-800 text-Gray-25'>Submit</button>
+                <Button
+                    size={'2xl'}
+                    type='submit'
+                    variant={'primary'}
+                    className='w-full'
+                >
+                    Sign in
+                </Button>
             </form>
         </div>
     )
 }
 
-export default LoginForm
+export default LoginForm;
