8186c0424feab7953861ab6ce416aa6100dfd65d
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-19 06:12:47 +0300

    Loading State for settings pages

---
+++ src/app/(dashboard)/(settings)/account-settings/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingSpinner from '@/components/layout/LoadingSpinner'
+import React from 'react'
+
+const loading = () => {
+    return (
+        <LoadingSpinner />
+    )
+}
+
+export default loading
---
+++ src/components/layout/LoadingSpinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LoadingSpinner = () => {
+    return (
+        <div className='w-full h-full flex items-center justify-center'>
+            <div
+                className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-Gray-300 border-e-Brand-600 align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status">
+                <span
+                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                >Loading...</span>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingSpinner
