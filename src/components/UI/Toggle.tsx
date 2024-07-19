edddbdb9a4d1e516ea28123d34db3af1024fcd61
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-19 06:07:11 +0300

    Toggle Shared Component

--- src/components/UI/Toggle.tsx
+++ src/components/UI/Toggle.tsx
@@ -3,46 +3,46 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from "@/app/lib/utils";
 
 const toggleVariants = cva(
-    "bg-Gray-100 p-[2px] h-fit inline-flex items-center rounded-full cursor-pointer transition-all",
+    `bg-Gray-100 
+    hover:bg-Gray-200 
+    has-[input:checked] 
+    has-[input:checked]:bg-Brand-600 
+    active:shadow-activeElementBoxShadow 
+    has-[input:disabled]:shadow-none 
+    has-[input:disabled]:cursor-not-allowed 
+    [&:has(input:checked)>span]:translate-x-full
+    p-[2px] 
+    h-fit 
+    inline-flex 
+    items-center 
+    rounded-full 
+    cursor-pointer 
+    transition-all
+    `,
     {
         variants: {
             size: {
                 sm: "w-9",
                 md: "w-11",
             },
-            disabled: {
-                true: "cursor-not-allowed",
-                false: "hover:bg-Gray-200 active:shadow-activeElementBoxShadow"
-            },
-            checked: {
-                true: "bg-Brand-600",
-                false: ""
-            },
         },
         defaultVariants: {
             size: 'md',
-            disabled: false,
-            checked: false
         },
     }
 );
 
 const circleVariants = cva(
-    "bg-white rounded-full transition-transform",
+    "bg-white rounded-full transform transition-transform translate-x-0",
     {
         variants: {
             size: {
                 sm: "w-4 h-4",
                 md: "w-5 h-5",
             },
-            checked: {
-                true: "transform translate-x-full",
-                false: "transform translate-x-0"
-            },
         },
         defaultVariants: {
             size: 'md',
-            checked: false
         },
     }
 );
@@ -55,25 +55,21 @@ interface IToggleProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>,
 
 const Toggle = forwardRef<HTMLInputElement, IToggleProps>(({
     size,
-    disabled,
-    checked,
     ...props
 }, ref) => {
     return (
-        <label className={cn(toggleVariants({ size, disabled, checked }))}>
+        <label className={cn(toggleVariants({ size }))}>
             <input
                 ref={ref}
                 type="checkbox"
                 className="sr-only"
-                disabled={disabled}
-                checked={checked}
                 {...props}
             />
             <span
                 style={{
                     filter: 'drop-shadow(0px 1px 2px rgba(16, 24, 40, 0.06)) drop-shadow(0px 1px 3px rgba(16, 24, 40, 0.10))'
                 }}
-                className={cn(circleVariants({ size, checked }))}
+                className={cn(circleVariants({ size }))}
             />
         </label>
     );
