d8b9a127a93a6e30c75f178ca7275ea78d7729d0
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-19 06:09:30 +0300

    Update Dashboard Layout Dimentions and Overflow State

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import Breadcrumb from "@/components/layout/Breadcrumb";
 import Sidebar from "@/components/layout/sidebar/Sidebar";
+import ModalWrapper from "@/components/modals/ModalWrapper";
+import { ModalProvider } from "@/context/ModalContext";
 
 import type { Metadata } from "next";
 
@@ -9,10 +11,9 @@ export const metadata: Metadata = {
 };
 
 export default function DashboardLayout({
-    children, params
+    children
 }: Readonly<{
     children: React.ReactNode,
-    params: any
 }>) {
 
     return (
@@ -20,9 +21,16 @@ export default function DashboardLayout({
             <div>
                 <Sidebar />
             </div>
-            <div className="flex flex-col items-start gap-10 bg-white rounded-tl-[2.5rem] flex-grow p-8">
-                <Breadcrumb />
-                {children}
+            <div className="flex flex-col items-start gap-10 bg-white rounded-tl-[2.5rem] flex-grow">
+                <div className="p-8 pb-0">
+                    <Breadcrumb />
+                </div>
+                <div className="w-full flex-grow overflow-y-auto p-8 pt-0">
+                    <ModalProvider>
+                        {children}
+                        <ModalWrapper />
+                    </ModalProvider>
+                </div>
             </div>
         </div>
     );
