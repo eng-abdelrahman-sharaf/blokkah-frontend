080caa471783a9ded15fc61072e485c63b98a94a
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-19 06:06:39 +0300

    New Icons Added

---
+++ src/components/icons/ImageIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ImageIcon = () => {
+    return (
+        <svg
+            width="56"
+            height="57"
+            viewBox="0 0 56 57"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect x="4" y="4.5" width="48" height="48" rx="24" fill="#F2F4F7" />
+            <rect x="4" y="4.5" width="48" height="48" rx="24" stroke="#F9FAFB" strokeWidth="8" />
+            <path
+                d="M21 37.5H35C36.1046 37.5 37 36.6046 37 35.5V21.5C37 20.3954 36.1046 19.5 35 19.5H21C19.8954 19.5 19 20.3954 19 21.5V35.5C19 36.6046 19.8954 37.5 21 37.5ZM21 37.5L32 26.5L37 31.5M26 25C26 25.8284 25.3284 26.5 24.5 26.5C23.6716 26.5 23 25.8284 23 25C23 24.1716 23.6716 23.5 24.5 23.5C25.3284 23.5 26 24.1716 26 25Z"
+                stroke="#475467"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    )
+}
+
+export default ImageIcon
