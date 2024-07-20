4cc5a58bbd9f3f4de4500b66cc5f19823de4ce66
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-07-20 05:41:10 +0300

    Add Success Color Variant, Update Font Sizes Tokens

--- src/app/lib/utils.tsx
+++ src/app/lib/utils.tsx
@@ -3,19 +3,13 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
-} 
+}
 
 export const fontSizesAliases = {
-    "display-2xl": "text-20xl",
-    "display-xl": "text-18xl",
-    "display-lg": "text-16xl",
-    "display-md": "text-14xl",
-    "display-sm": "text-12xl",
-    "display-xs": "text-10xl",
-    "text-xl": "text-5xl",
-    "text-lg": "text-4xl",
-    "text-md": "text-3xl",
-    "text-sm": "text-2xl",
-    "text-xs": "text-xl",
-  };
-  
+    "display-2xl": "text-7xl",
+    "display-xl": "text-6xl",
+    "display-lg": "text-5xl",
+    "display-md": "text-4xl",
+    "display-sm": "text-3xl",
+    "display-xs": "text-2xl",
+};
