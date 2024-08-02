32dc978eabb170da803c708d046815c512edb523
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-08-02 03:29:33 +0300

    render custom component if exists for dashboard pages header

--- src/components/layout/DashboardPagesHeader.tsx
+++ src/components/layout/DashboardPagesHeader.tsx
@@ -8,8 +8,9 @@ const DashboardPagesHeader = (
         description,
         customIconComponent = undefined,
         buttonText = undefined,
-        onClick = undefined
-    }: { title: string, description?: string, customIconComponent?: ReactNode, buttonText?: string, onClick?: () => void }) => {
+        onClick = undefined,
+        endComponent
+    }: { title: string, description?: string, customIconComponent?: ReactNode, buttonText?: string, onClick?: () => void, endComponent?: React.ReactNode }) => {
 
     return (
         <div className='flex items-center justify-between w-full'>
@@ -17,16 +18,18 @@ const DashboardPagesHeader = (
                 <h1 className='font-bold text-Gray-900 text-xl sm:text-2xl md:text-3xl'>{title}</h1>
                 <p className='text-xs sm:text-sm md:text-md font-regular text-Gray-600'>{description}</p>
             </div>
-            <Button
-                size={'md'}
-                type='submit'
-                icon='leading'
-                customIconComponent={customIconComponent}
-                onClick={onClick}
-                className='w-fit text-xs sm:text-sm md:text-md px-2 py-1 sm:px-3 sm:py-2 md:px-4 md:py-[0.625rem]'
-            >
-                {buttonText}
-            </Button>
+            {endComponent ? endComponent : (
+                <Button
+                    size={'md'}
+                    type='submit'
+                    icon='leading'
+                    customIconComponent={customIconComponent}
+                    onClick={onClick}
+                    className='w-fit text-xs sm:text-sm md:text-md px-2 sm:px-3 py-2 md:px-4 md:py-[0.625rem]'
+                >
+                    {buttonText}
+                </Button>
+            )}
         </div>
     )
 }
