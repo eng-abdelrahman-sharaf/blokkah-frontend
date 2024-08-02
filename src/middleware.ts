92094cd5332d22bbc372adb09865ab8186c5a900
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-08-02 03:34:43 +0300

    Add Middleware to redirect when matching specific routes

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+// middleware.ts
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const redirects: { [key: string]: string } = {
+    '/user-management': '/user-management/agents-requests',
+    '/property-management': '/property-management/property-types',
+    '/content-management': '/content-management/banners',
+    '/reports-and-complaints': '/reports-and-complaints/agents-and-properties',
+};
+
+export function middleware(request: NextRequest) {
+    const url = request.nextUrl.clone();
+    const pathname = url.pathname;
+
+    if (redirects[pathname]) {
+        url.pathname = redirects[pathname];
+        return NextResponse.redirect(url);
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/user-management',
+        '/property-management',
+        '/content-management',
+        '/reports-and-complaints',
+    ],
+};
