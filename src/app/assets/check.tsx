d5b443a86f6ba718adf5509dd3d6c703f4a9ee36
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-08-26 13:55:58 +0300

    adding checkbox component

---
+++ src/app/assets/check.tsx
@@ -0,0 +1,20 @@
+function Check({className}:{className?: string}) {
+  return (
+    <svg
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M10 3L4.5 8.5 2 6"
+        stroke="#002949"
+        strokeWidth={1.6666}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Check
