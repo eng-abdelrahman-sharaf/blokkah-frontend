b25721e730e9c36dfbf3a78123ada37bc67f695e
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-01 21:13:25 +0300

    making radioGroup as a separate component

--- src/app/(testing)/radio/page.tsx
+++ src/app/(testing)/radio/page.tsx
@@ -1,49 +1,5 @@
+import RadioGroup from "@/app/buyer/home/components/radioGroup";
 import { cn } from "@/lib/utils";
-
-const RadioGroup = ({
-  ValueArray,
-  nodesArray,
-  name,
-  checkedValue = "",
-  singleContainerClassName,
-  wholeContainerClassName,
-}: {
-  ValueArray: Array<string>;
-  nodesArray?: Array<JSX.Element>;
-  name: string;
-  checkedValue?: string;
-  wholeContainerClassName?: string;
-  singleContainerClassName?: string;
-}) => {
-  return (
-    <div
-      className={cn(
-        "flex p-3 flex gap-3 bg-white rounded-lg",
-        wholeContainerClassName
-      )}
-    >
-      {ValueArray.map((value, index) => {
-        return (
-          <label
-            key={index}
-            className={`text-Gray-500 text-lg font-regular  rounded-lg  px-3 py-2 relative
-                [&:has(>:checked)]:bg-Secondary-50 [&:has(>:checked)]:text-Secondary-900`}
-          >
-            {nodesArray && nodesArray[index]}
-            <div className="absolute w-full h-full top-0 left-0 rounded-lg [&:has(+:checked)]:border [&:has(+:checked)]:border-Secondary-600"></div>
-            <input
-              name={name}
-              value={value}
-              className={`hidden`}
-              type="radio"
-              defaultChecked={value == checkedValue ? true : undefined}
-            ></input>
-          </label>
-        );
-      })}
-    </div>
-  );
-};
 
 export default function Page() {
   return (
@@ -51,9 +7,11 @@ export default function Page() {
       <RadioGroup
         name="property-type"
         ValueArray={["rent", "buy"]}
-        nodesArray={[<div>Rent</div>, <div>Buy</div>]}
         checkedValue="buy"
-      />
+      >
+        <div>Rent</div>
+        <div>Buy</div>
+      </RadioGroup>
     </div>
   );
 }
