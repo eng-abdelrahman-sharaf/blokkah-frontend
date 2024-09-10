e375045fc9de5af5e80ed8d371af2959d4719346
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-10 20:20:56 +0300

    making the DropDownBody Button customizable

--- src/app/buyer/components/dropDown.tsx
+++ src/app/buyer/components/dropDown.tsx
@@ -1,73 +1,81 @@
-import { Button } from "@/components/UI/Button";
+"use client";
+
+import { Button, buttonVariants } from "@/components/UI/Button";
+import { cn } from "@/lib/utils";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { ReactNode, useState } from "react";
+import { ButtonProps } from "@/components/UI/Button";
 
 interface DropDownProps {
   dropDownText: string;
   AbsoluteMenu: ReactNode;
-
   className?: string;
-  buttonClassName?: string;
+
+  buttonProps?: ButtonProps
+
 }
 
-export function DropDownBody({
-  dropDownText,
-  AbsoluteMenu,
-  className,
-  buttonClassName,
-}: DropDownProps) {
+export function DropDownBody(props: DropDownProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const {
+    variant = "dropDownTrigger",
+    icon = "trailing",
+    customIconComponent =           isOpen ? (
+      <ChevronUp className="h-4 w-4" />
+    ) : (
+      <ChevronDown className="h-4 w-4" />
+    ),
+    className: buttonClass,
+    childrenWrapperClassName,
+    onClick = () => setIsOpen(!isOpen),
+  } = props.buttonProps!;
+
 
   return (
-    <div className="relative inline-block text-left">
+    <div className={cn("relative inline-block text-left", props.className)}>
       <Button
-        variant={"dropDownTrigger"}
-        icon={"trailing"}
-        customIconComponent={
-          isOpen ? (
-            <ChevronUp className="h-4 w-4" />
-          ) : (
-            <ChevronDown className="h-4 w-4" />
-          )
-        }
-        className="gap-8 w-[200px]"
-        childrenWrapperClassName="grow overflow-hidden text-ellipsis"
-        onClick={() => setIsOpen(!isOpen)}
+        variant={variant as any}
+        icon={icon as any}
+        customIconComponent={customIconComponent}
+        className={buttonClass}
+        childrenWrapperClassName={childrenWrapperClassName}
+        onClick={onClick}
       >
-        {dropDownText}
+        {props.dropDownText}
       </Button>
 
-      {isOpen && AbsoluteMenu}
+      {isOpen && props.AbsoluteMenu}
     </div>
   );
 }
 
-const RangeInput = ({onChange}:{onChange:any}) => {
+const RangeInput = ({ onChange }: { onChange: any }) => {
   return (
     <input
       type="number"
       className="placeholder:text-md placeholder:font-regular bg-white border border-Gray-300 shadow-xs rounded-lg py-2.5 px-3.5 w-full text-Gray-700 h-fit"
       placeholder="0"
-  />
+    />
   );
-} 
+};
 const RangeAbsMenu = () => {
-  return <div className="absolute bg-white rounded-lg border border-Gray-100 top-[100%] translate-y-2.5">
+  return (
+    <div className="absolute bg-white rounded-lg border border-Gray-100 top-[100%] translate-y-2.5">
       <div className="flex gap-4 py-1 px-8 ">
         <div className="flex gap-1 text-md font-medium">
-        <div className="flex flex-col items-start gap-1.5">
-          <div>Minimum</div>
-          <RangeInput onChange={()=>{}} />
+          <div className="flex flex-col items-start gap-1.5">
+            <div>Minimum</div>
+            <RangeInput onChange={() => {}} />
           </div>
+
           <div></div>
         </div>
         <div></div>
       </div>
-    </div>;
-}
+    </div>
+  );
+};
 
 export function AreaDropDown() {
-  return (
-    <DropDownBody dropDownText="Area" AbsoluteMenu={<RangeAbsMenu />} />
-  );
+  return <DropDownBody dropDownText="Area" AbsoluteMenu={<RangeAbsMenu />} />;
 }
