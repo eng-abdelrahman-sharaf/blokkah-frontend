20d2829714cc485046d5edbf9c5caf7ce580a9fa
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-11 22:23:53 +0300

    passing strings instead of requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,58 @@
-// middleware.ts
-
+// // middleware.ts
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { internMiddleware, internMatcher } from "@/lib/internationalization";
+import { NextRequest } from "next/server";
 import {
-  dashboardMiddleware,
-  dashboardMatcher,
-} from "@/lib/dashboardMiddleWare";
+  getPathNameWithoutLocale,
+  getPathNameLocale,
+  addLocaleToPathName,
+} from "@/lib/internationalization";
+import { dashboardMiddleware } from "@/lib/dashboardMiddleWare";
 
 export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
+  const url = request.nextUrl.clone();
+  const pathname: string = url.pathname;
+  let pathNameWithoutLocale = pathname;
+  const locale = getPathNameLocale(pathname);
 
-  if (dashboardMatcher.includes(pathname)) {
-    return dashboardMiddleware(request);
-  } else if (pathname.startsWith("/buyer")) {
-    return internMiddleware(request);
-  } else return NextResponse.next();
+  console.log("middleWare entered");
+
+  if (locale) {
+    pathNameWithoutLocale = getPathNameWithoutLocale(pathname);
+  }
+
+  // this line is in case that the dashboard middleware will not be used
+  let finalPathname = pathNameWithoutLocale;
+
+  // // applying dashboard middleware on pathName without Locale
+  // let finalPathname = dashboardMiddleware(pathNameWithoutLocale);
+
+  // if no need to apply middleware
+  if (locale && finalPathname == pathNameWithoutLocale)
+    return NextResponse.next();
+
+  // adding locale to the pathname
+  finalPathname = addLocaleToPathName(finalPathname, locale);
+
+  url.pathname = finalPathname;
+
+  return NextResponse.redirect(url);
 }
 
 export const config = {
-    matcher: internMatcher.concat(dashboardMatcher),
+  matcher: [
+      /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next (all internal paths)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - ar, en (language routes)
+     */
+    '/((?!api|_next|favicon.ico|sitemap.xml|robots.txt|ar|en).*)',
+
+    // // match all dashboard end routes either starting with locale or not
+    // '/(.*)/user-management',
+    // '/(.*)/property-management',
+    // '/(.*)/content-management',
+    // '/(.*)/reports-and-complaints',
+  ],
 };
