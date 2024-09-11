5ab1852297205f93a853c716f41ee5cb3d95bcec
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-11 22:25:39 +0300

    suggesting a better way of redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// a better way for redirecting without using middleware
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/:locale+/user-management',
+            destination: '/:locale+/user-management/agents-requests',
+            permanent: true,
+          },
+          {
+            source: '/:locale+/property-management',
+            destination: '/:locale+/property-management/property-types',
+            permanent: true,
+          },
+          {
+            source: '/:locale+/content-management',
+            destination: '/:locale+/content-management/banners',
+            permanent: true,
+          },
+          {
+            source: '/:locale+/reports-and-complaints',
+            destination: '/:locale+/reports-and-complaints/agents-and-properties',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
