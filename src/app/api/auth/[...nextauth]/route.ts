12e507d712208fd6037765483e82e046d3a33ba5
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-16 17:11:06 +0300

    adding session custom callback authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,30 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            id: "google",
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            authorization: {
-                params: {
-                  prompt: "consent",
-                  access_type: "offline",
-                  response_type: "code"
-                }
-            },
-          })
-    ],
-})
+export const authOptions = NextAuth({
+  providers: [
+    GoogleProvider({
+      id: "google",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
 
-export { handler as GET, handler as POST }
+  callbacks: {
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+});
+
+export { authOptions as GET, authOptions as POST };
