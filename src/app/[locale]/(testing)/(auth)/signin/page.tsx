7702d03e2fa6d5de189163b5f3a92cea1d248862
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-16 03:28:04 +0300

    testing sign in

--- src/app/[locale]/(testing)/(auth)/signin/page.tsx
+++ src/app/[locale]/(testing)/(auth)/signin/page.tsx
@@ -1,14 +1,23 @@
-"use client"
+"use client";
 
 import GoogleIcon from "@/app/[locale]/buyer/assets/googleIcon";
 import { Button } from "@/components/UI/Button";
 import { url } from "inspector";
-import {signIn} from "next-auth/react"
+import { signIn } from "next-auth/react";
 
 export default function Page() {
-    return (
-        <Button icon="leading" variant={"secondaryGray"} size={"lg"} customIconComponent={<GoogleIcon className="w-5"/>}  className="w-[70%]" onClick={()=>{signIn("google" , {callbackUrl:"/carousel" , redirect:false })}}>
-            Sign in in with Google
-        </Button>
-    )
- }
+  return (
+    <Button
+      icon="leading"
+      variant={"secondaryGray"}
+      size={"lg"}
+      customIconComponent={<GoogleIcon className="w-5" />}
+      className="w-[70%]"
+      onClick={() => {
+        signIn("google", { callbackUrl: "/", redirect: false });
+      }}
+    >
+      Sign in in with Google
+    </Button>
+  );
+}
