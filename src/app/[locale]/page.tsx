e3d897cc7abb3f7bd0bb903452f79dc77e453af8
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-09-16 03:26:58 +0300

    trying to get session gmail

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,17 @@
-export default function Home() {
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import image from "@/app/[locale]/buyer/assets/building.png"
+import Background from './components/background';
+
+export default async function Home() {
+  // Fetch session on the server
+  const session : any = await getServerSession(authOptions);
   return (
     <main>
+      <Background imageSrc={image.src}>
+        {session?.user?.email}
+
+      </Background>
     </main>
   );
 }
