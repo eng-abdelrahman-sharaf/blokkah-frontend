6775afd048928d2ec3b651154b7ea06d9a6d2905
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-10-12 17:03:42 +0300

    fixing errors in radio page

--- src/app/[locale]/(testing)/radio/page.tsx
+++ src/app/[locale]/(testing)/radio/page.tsx
@@ -7,8 +7,10 @@ import AIChatIcons from "../../buyer/home/assets/aiChatIcons";
 import PropertyRequestIcons from "../../buyer/home/assets/propertyRequestIcons";
 import FAQIcons from "../../buyer/home/assets/FAQIcons";
 
-const Option = ({ text, icons }: { text: string; icons: ReactNode }) => (
-  <div>
+// props is used to get data-checked
+const Option = ({ text, icons , ...props }: { text: string; icons: ReactNode }) => (
+  // if this input is checked = "data-[checked=true]:"
+  <div className="[&[data-checked=true]>*>*:first-child]:hidden [&[data-checked=true]>*>*:nth-child(2)]:block" {...props}>
     <div className="flex items-center gap-0.5 flex-col">
       {icons}
       <div className="text-center text-md font-semibold">{text}</div>
@@ -17,49 +19,32 @@ const Option = ({ text, icons }: { text: string; icons: ReactNode }) => (
 );
 
 export default function Page() {
+  const BotSelectionState = useState("");
+  const saleValueArray = ["sale", "rent"];
+  const SaleCheckedValue = useState(saleValueArray[0]);
+
   return (
     <div className="w-[70%]">
       {/* Basic Usage that takes 100% from the width */}
-      <RadioGroup
-        checkedValueState={useState("sale")}
-        ValueArray={["sale", "rent"]}
-        name="req-type"
-        singleContainerClassName="py-3 bg-Gray-25 w-[50%]"
-        wholeContainerClassName="gap-0 p-0"
-      >
-        <div className="max-w-[max-content]">for Sale</div>
-        <div className="max-w-[max-content]">for Rent</div>
-      </RadioGroup>
-
+      
+      {/* Bot selection */}
       {/* using different icons to represent radio Group State */}
       <RadioGroup
-        checkedValueState={useState("")}
+        checkedValueState={BotSelectionState}
         name="chatbot-section"
         ValueArray={["0", "1", "2"]}
-        onLabelChecked={(label: HTMLLabelElement) => {
-          const svgs = label.querySelectorAll("svg");
-          svgs[0]?.classList?.add("hidden");
-          svgs[1]?.classList?.remove("hidden");
-        }}
-        onLabelUnChecked={(label: HTMLLabelElement) => {
-          const svgs = label.querySelectorAll("svg");
-          svgs[0]?.classList?.remove("hidden");
-          svgs[1]?.classList?.add("hidden");
-        }}
+        singleContainerClassName="data-[checked=true]:border-0 data-[checked=true]:bg-transparent"
         wholeContainerClassName="gap-0 justify-evenly rounded-2xl"
       >
-        <Option
-          icons={<AIChatIcons className="h-10 aspect-square" />}
-          text="AI Chat"
-        />
-        <Option
-          icons={<PropertyRequestIcons className="h-10 aspect-square" />}
-          text="Property Request"
-        />
-        <Option
-          icons={<FAQIcons className="h-10 aspect-square" />}
-          text="FAQ"
-        />
+        <Option icons={<AIChatIcons className="h-10 aspect-square" />} text="AI Chat" />
+        <Option icons={<PropertyRequestIcons className="h-10 aspect-square"/>} text="Property Request" />
+        <Option icons={<FAQIcons className="h-10 aspect-square"/>} text="FAQ" />
+      </RadioGroup>
+
+      {/* Simple usage */}
+      <RadioGroup ValueArray={saleValueArray} checkedValueState={SaleCheckedValue} name="req-type" singleContainerClassName="py-3 bg-Gray-25 w-[50%]" wholeContainerClassName="gap-0 p-0">
+                <div className="max-w-[max-content]">for Sale</div>
+                <div className="max-w-[max-content]">for Rent</div>
       </RadioGroup>
     </div>
   );
