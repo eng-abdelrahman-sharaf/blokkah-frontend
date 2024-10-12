9cbd5b90a53ae9ab3be400e1d7c4dc14966fe773
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-10-12 16:53:31 +0300

    adding radio used in pages inside the radio page as examples

--- src/app/[locale]/(testing)/radio/page.tsx
+++ src/app/[locale]/(testing)/radio/page.tsx
@@ -1,16 +1,65 @@
+"use client";
+
 import RadioGroup from "@/app/[locale]/buyer/home/components/radioGroup";
 import { cn } from "@/lib/utils";
+import { ReactNode, useState } from "react";
+import AIChatIcons from "../../buyer/home/assets/aiChatIcons";
+import PropertyRequestIcons from "../../buyer/home/assets/propertyRequestIcons";
+import FAQIcons from "../../buyer/home/assets/FAQIcons";
+
+const Option = ({ text, icons }: { text: string; icons: ReactNode }) => (
+  <div>
+    <div className="flex items-center gap-0.5 flex-col">
+      {icons}
+      <div className="text-center text-md font-semibold">{text}</div>
+    </div>
+  </div>
+);
 
 export default function Page() {
   return (
-    <div>
+    <div className="w-[70%]">
+      {/* Basic Usage that takes 100% from the width */}
       <RadioGroup
-        name="property-type"
-        ValueArray={["rent", "buy"]}
-        defaultChecked="buy"
+        checkedValueState={useState("sale")}
+        ValueArray={["sale", "rent"]}
+        name="req-type"
+        singleContainerClassName="py-3 bg-Gray-25 w-[50%]"
+        wholeContainerClassName="gap-0 p-0"
       >
-        <div>Rent</div>
-        <div>Buy</div>
+        <div className="max-w-[max-content]">for Sale</div>
+        <div className="max-w-[max-content]">for Rent</div>
+      </RadioGroup>
+
+      {/* using different icons to represent radio Group State */}
+      <RadioGroup
+        checkedValueState={useState("")}
+        name="chatbot-section"
+        ValueArray={["0", "1", "2"]}
+        onLabelChecked={(label: HTMLLabelElement) => {
+          const svgs = label.querySelectorAll("svg");
+          svgs[0]?.classList?.add("hidden");
+          svgs[1]?.classList?.remove("hidden");
+        }}
+        onLabelUnChecked={(label: HTMLLabelElement) => {
+          const svgs = label.querySelectorAll("svg");
+          svgs[0]?.classList?.remove("hidden");
+          svgs[1]?.classList?.add("hidden");
+        }}
+        wholeContainerClassName="gap-0 justify-evenly rounded-2xl"
+      >
+        <Option
+          icons={<AIChatIcons className="h-10 aspect-square" />}
+          text="AI Chat"
+        />
+        <Option
+          icons={<PropertyRequestIcons className="h-10 aspect-square" />}
+          text="Property Request"
+        />
+        <Option
+          icons={<FAQIcons className="h-10 aspect-square" />}
+          text="FAQ"
+        />
       </RadioGroup>
     </div>
   );
