d16c10f7a106d7201df2f1c5c8702574dc190e73
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-10-19 22:18:02 +0300

    adding background className for gradients and backgrounds

--- src/app/[locale]/components/background.tsx
+++ src/app/[locale]/components/background.tsx
@@ -7,17 +7,20 @@ import React from "react";
 interface BackgroundProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
     className?: string;
     imgClassName?: string;
+    backgroundClassName?: string;
     imageSrc: string;
     imageAlt?: string;
 }
 const Background = React.forwardRef<
     HTMLDivElement,
     BackgroundProps
-    >(({imageSrc , imageAlt = "background-image", imgClassName ,  className , ...props}, ref) => {
+    >(({imageSrc , imageAlt = "background-image",backgroundClassName , imgClassName ,  className , ...props}, ref) => {
     return (
-        <div ref={ref} className={cn("relative [&>*:not(:last-child)]:z-0 [&>*:not(:last-child)]:relative", className)} {...props}>
+        <div ref={ref} className={cn('relative z-0 [&>*:not(:nth-last-child(-n+2))]:z-0 [&>*:not(:nth-last-child(-n+2))]:relative', className)} {...props}>
             {props.children}
-            <Image src={imageSrc} alt={imageAlt} className={cn("absolute top-0 left-0 right-0 bottom-0 object-cover w-full h-full -z-20" , imgClassName)} fill />
+            {/* for linear gradients, shade ...etc */}
+            <div className={cn("absolute top-0 left-0 right-0 bottom-0 -z-10" , backgroundClassName)} data-background={true}></div>
+            <Image src={imageSrc} alt={imageAlt} className={cn("absolute top-0 left-0 right-0 bottom-0 object-cover w-full h-full -z-20", imgClassName)} fill data-background={true} />
         </div>
     )
 });
