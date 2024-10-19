02650584a7a8b0aa85f6c29aaa18781d23f060ef
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-10-19 22:45:18 +0300

    fix: menu distance from the dropdown

--- src/app/[locale]/(user-flow)/(home)/components/searchComponents/MenuContainer.tsx
+++ src/app/[locale]/(user-flow)/(home)/components/searchComponents/MenuContainer.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const MenuContainer = ({children}:{children?:ReactNode}) => (
-  <div className="py-3 bg-white border border-Gray-100 rounded-lg absolute top-full translate-y-3 z-20">
+  <div className="py-3 bg-white border border-Gray-100 rounded-lg absolute top-full translate-y-2 z-20">
     {children}
   </div>
 )
