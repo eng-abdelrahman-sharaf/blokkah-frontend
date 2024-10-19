e1093e6368fcb47ace9181e8c780de6ea2dc88bc
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-10-19 22:20:35 +0300

    unfixing a layout size

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import BuildingBG from "./components/buildingBG";
 
 export default function Layout({children}:{children:React.ReactNode}) { 
     return (
-        <div className="w-full h-full min-h-96 flex">
+        <div className="w-full flex">
             <BuildingBG/>
             <AuthCardContainer> 
                 {children}
